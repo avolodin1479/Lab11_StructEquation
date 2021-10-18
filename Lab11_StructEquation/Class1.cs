227983660247c6844f6b607e683d7c2e6195b05c
avolodin1479@gmail.com <avolodin1479@gmail.com>
2021-10-18 09:28:55 +0600

    Добавьте файлы проекта.

---
+++ Lab11_StructEquation/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11_StructEquation
+{
+    public struct Equation
+    {
+        public double coefK;
+        public double coefB;
+
+        public Equation(double coefK, double coefB)
+        {
+            this.coefK = coefK;
+            this.coefB = coefB;
+        }
+        public void Root(double coefK, double coefB, out double rootX)
+        {
+            rootX = -coefB / coefK;
+        }
+    }
+
+}
